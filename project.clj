190e6abda55aacb1cda42533f78984d2be9bdc9a
gered <gered@blarg.ca>
2017-09-05 09:05:21 -0400

    version 0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-web-app/lein-template "0.1"
+(defproject simple-web-app/lein-template "0.2"
   :description       "Simple / Stripped-down template for Clojure/ClojureScript web apps"
   :url               "https://github.com/gered/simple-web-app-template"
   :license           {:name "MIT License"
